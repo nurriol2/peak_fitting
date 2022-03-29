b939298189ac03e6a6f520d0a5c9cc544da24830
nurriol2 <nurriol2@illinois.edu>
2022-03-29 03:10:27 -0500

    Fixed:  Selecting a column returns an array instead of a pandas.Series

--- data_toolbox/workflows/hetr_sideband_workflow.py
+++ data_toolbox/workflows/hetr_sideband_workflow.py
@@ -1,7 +1,16 @@
 import os
+from select import select
 import numpy as np
 import pandas as pd
+from data_toolbox.allan_func.allan import overlapping_allan_deviation as oadev
+from data_toolbox.allan_func.coefficient_fitting import fit_bias_instability_line
+from data_toolbox.allan_func.coefficient_fitting import fit_random_walk_line
+from data_toolbox.allan_func.coefficient_fitting import fit_rate_random_walk_line
 
+
+# Column names to use for the DataFrame
+COLUMNS = ["area_x", "freq_x", "linewidth_x", "area_y", "freq_y", "linewidth_y", "residuals"]
+SAMPLING_RATE = 1/326.613
 
 def _process(dir, filename):
     
@@ -15,9 +24,6 @@ def _process(dir, filename):
     Returns:
         (pandas.DataFrame):  DataFrame from the contents of a sideband data file.
     """
-
-    # Column names to use for the DataFrame
-    COLUMNS = ["area_x", "freq_x", "linewidth_x", "area_y", "freq_y", "linewidth_y", "residuals"]
     
     # Check that the filename specifies .dat type
     if not filename.lower().endswith(".dat"):
@@ -31,8 +37,38 @@ def _process(dir, filename):
     # Convert sideband data into a DataFrame
     return pd.DataFrame(data=sideband_array, columns=COLUMNS)
 
+def _select_column(dir, filename, parameter):
+    
+    """
+    Select a specified column from the sideband DataFrame.
 
+    Raises:
+        Exception: When the supplied parameter is not one of the column names.
+
+    Returns:
+        numpy.ndarray: Column of the DataFrame as an array.
+    """
+
+    # Check that the parameter supplied is a column of the DataFrame
+    if parameter not in COLUMNS:
+        raise Exception(f"{parameter} not found in {COLUMNS}.")
+    
+    
+    df = _process(dir, filename)
+    selected_values = df[parameter]
+    return selected_values.to_numpy()
 
 def run_workflow(dir, filename, parameter):
-    pass
-    return 
+    
+    # Measured signal of the time series
+    signal = _select_column(dir, filename, parameter)
+    
+    # Compute the Allan deviation of the signal
+    t, s = oadev(signal, SAMPLING_RATE)
+
+    # Estimate the coefficients 
+    bias_instability_coeff = fit_bias_instability_line(t, s)
+    rate_random_walk_coeff = fit_rate_random_walk_line(t, s)
+    random_walk_coeff = fit_random_walk_line(t, s)
+
+    return bias_instability_coeff, rate_random_walk_coeff, random_walk_coeff
