8f667d557af1d6e8b2e5787325cb28bf0e4b6c52
nurriol2 <nurriol2@illinois.edu>
2022-03-29 02:48:06 -0500

    Add documentation

--- data_toolbox/workflows/hetr_sideband_workflow.py
+++ data_toolbox/workflows/hetr_sideband_workflow.py
@@ -3,11 +3,14 @@ import numpy as np
 import pandas as pd
 
 
-
 def _process(dir, filename):
-
+    
     """
     Load the sideband data from a .dat file. Then, create a DataFrame its contents.
+
+    Args:
+        dir (str): Parent directory of the sideband data
+        filename (str):  Desired sideband data file. Must be .dat suffix.
 
     Returns:
         (pandas.DataFrame):  DataFrame from the contents of a sideband data file.
@@ -16,6 +19,10 @@ def _process(dir, filename):
     # Column names to use for the DataFrame
     COLUMNS = ["area_x", "freq_x", "linewidth_x", "area_y", "freq_y", "linewidth_y", "residuals"]
     
+    # Check that the filename specifies .dat type
+    if not filename.lower().endswith(".dat"):
+        raise Exception(f"{filename} must specify the `.dat` extension.")
+
     # Full path to the specific sideband file
     path2data = os.path.join(dir, filename)
 
