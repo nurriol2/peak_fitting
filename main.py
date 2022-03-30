fe033f3fa83f0b497e4e4e9cd121b9c1769376ef
nurriol2 <nurriol2@illinois.edu>
2022-03-30 06:59:39 -0500

    Demonstrate targeting heterodyne data using CustomWorkflow

--- main.py
+++ main.py
@@ -25,47 +25,28 @@ def main():
     # Target the negative sideband of the heterodyne data set
     ucl.target_heterodyne(which_sideband="neg", mode='x')
     # View the reports
-    #print(ucl.area_report) 
-    #print(ucl.freq_report) 
-    #print(ucl.line_report) 
+    # print(ucl.area_report) 
+    # print(ucl.freq_report) 
+    # print(ucl.line_report) 
     
     ### Custom Analysis ###
     
     ## Split Detection Data ##
     # "cha" <--> "x" and "chb" <--> "y" throughout
     cst = CustomWorkflow()
-    # This runs "cha" data
+    # This runs "chb" data
     cst.target_split_detection(mode='y')
+    # print(cst.area_report) 
+    # print(cst.freq_report) 
+    # print(cst.line_report) 
+    
+
+    cst = CustomWorkflow()
+    cst.target_heterodyne(which_sideband="pos")
     print(cst.area_report) 
     print(cst.freq_report) 
     print(cst.line_report) 
-    
-"""
 
-    # Whereas, this runs the "chb" data
-    cst_split_y = CustomWorkflow.split_detection(mode="y")
-    cst_split_y.area_report()
-    cst_split_y.freq_report()
-    cst_split_y.line_report()
-
-    ## Heterodyne Data ##
-    # Positive and negative sideband data both come from the same `het_st80_.CSV` files
-
-    # For positive, you have to `Lorentzian.fit_right_peak()`
-    cst_pos = CustomWorkflow.hetr_sideband(which_sideband="positive")
-    cst_pos.area_report()
-    cst_pos.freq_report()
-    cst_pos.line_report() 
-
-    # For negative, you do `Lorentzian.fit_left_peak()`
-    cst_neg = CustomWorkflow.hetr_sideband(which_sideband="negative")
-    cst_neg.area_report()
-    cst_neg.freq_report()
-    cst_neg.line_report() 
-
-
-    return 
-"""
 
 if __name__=="__main__":
     main()
