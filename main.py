53342036eceb48538417a8a25a3d8333ca07d7fb
nurriol2 <nurriol2@illinois.edu>
2022-04-04 23:52:17 -0500

    Create objs

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ def main():
     ucl = UCLWorkflow()
     cst = CustomWorkflow()
 
-    # Target split detection data set
-    ucl.target_split_detection('x')
-    cst.target_split_detection('x')
-
-    # Target heterodyne data set
-    #ucl.target_heterodyne("pos", "y")
-    #cst.target_heterodyne("pos")
-
-    print(ucl.freq_report)
-    print(cst.line_report)
-
     return
 
 if __name__=="__main__":
