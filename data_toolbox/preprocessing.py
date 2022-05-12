27e97af7ae40b4dd5992b40f3045b0dd978d535c
nurriol2 <nurriol2@illinois.edu>
2022-05-12 16:22:19 -0500

    Add preprocessing for UCL heterodyne data

--- data_toolbox/preprocessing.py
+++ data_toolbox/preprocessing.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import pandas as pd
 import numpy as np 
 import pathlib
@@ -8,6 +9,38 @@ logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 #logging.disable(logging.CRITICAL)
 
 
+### General Helper Functions ###
+
+abbreviate_sideband = lambda sideband: sideband.lower().strip()[0:3]
+
+def load_raw_sideband(sideband):
+    """
+    Load a `fit_{sideband}_sideband.dat` file into memory as a dataframe.
+    
+    Assign the column labels according to the ReadMe.txt. 
+    Since the ReadMe does not specify the 7th column label, the column
+    label is assigned by this function as `error`. 
+    
+    Args:
+        sideband (str, optional):  Heterodyne sideband identifier. 
+
+    Returns:
+        (pandas.DataFrame):  A `fit_{sideband}_sideband.dat` file as a dataframe.
+    """
+
+    ### Point to the file ###
+    # Directory of all raw UCL data
+    ucl_fits_path = constants.RAW_DATA_DIRECTORY.joinpath("raw_ucl_fits")
+    # Format the filename
+    raw_filename_templ = f"fit_{abbreviate_sideband(sideband)}_sideband.dat"
+    # Full path to the raw data
+    target = ucl_fits_path.joinpath(raw_filename_templ)
+    
+    ### Format the dataframe ###
+    col_names = ["Area_x", "f_x", "linewidth_x", "Area_y", "f_y", "linewidth_y", "error"]
+    raw_df = pd.read_csv(target, names=col_names, sep="\t")
+
+    return raw_df
 
 def clean_path(source, mode, sideband=None):
 
@@ -50,7 +83,9 @@ def clean_path(source, mode, sideband=None):
     return filepath
 
 
-def ucl_split_detection(mode):
+
+### Data Source Specific Preproccessing Functions ### 
+def preprocess_ucl_split_detection(mode):
 
     """
     Preprocess raw split detection data into a CSV for the specified mode.
@@ -99,6 +134,33 @@ def ucl_split_detection(mode):
 
     return 
 
+def preprocess_ucl_heterodyne(mode, sideband):
+
+    ### Locate raw data file ###
+    # Load the corresponding heterodyne fit into memory as a dataframe
+    df = load_raw_sideband(sideband=sideband)
+    # Filter the columns by the directional mode
+    matched_df = df.filter(regex=f"\w+{mode}")
+
+    ### Rename the matched column names to the standard set of names ###
+    current_names = list(matched_df.columns)
+    standard_names = ["area_under_curve" ,"mechanical_frequency" ,"linewidth"]
+    name_map = {curr:std for curr,std in list(zip(current_names, standard_names))}
+    logging.debug(name_map)
+    matched_df = matched_df.rename(columns=name_map)
+    logging.debug(f"Matched names: {matched_df.columns}")
+
+    # Create data for time step column
+    time_step = np.arange(0, len(matched_df)) * constants.SPLIT_DETECTION_SAMLPING_STEP_SIZE
+    # Insert the time step column into the matching dataframe
+    matched_df.insert(0, "time_step", time_step)
+
+    ### Write dataframe to file ###
+    target = clean_path(source="ucl", mode=mode, sideband=sideband)
+    logging.debug(f"Target file {target}")
+    matched_df.to_csv(target, index_label="index")
+    
+    return 
+
 if __name__=="__main__":
-    ucl_split_detection('x')
-    ucl_split_detection('y')
+    preprocess_ucl_heterodyne('x', "negative")
