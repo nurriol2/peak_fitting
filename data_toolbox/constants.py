ffa6fdc7fd312e134ae4246d830b9224c98cf410
nurriol2 <nurriol2@illinois.edu>
2022-05-12 00:35:32 -0500

    Add file for importable constants

---
+++ data_toolbox/constants.py
@@ -0,0 +1,13 @@
+import pathlib
+
+
+
+### Directory paths ###
+PACKAGE_DIRECTORY = pathlib.Path(__file__).parents[1]
+EXPERIMENT_DATA_DIRECTORY = PACKAGE_DIRECTORY.joinpath("experiment_data")
+RAW_DATA_DIRECTORY = EXPERIMENT_DATA_DIRECTORY.joinpath("raw_data")
+CLEAN_DATA_DIRECTORY = EXPERIMENT_DATA_DIRECTORY.joinpath("clean_data")
+
+### Sampling ###
+HETERODYNE_SAMPLING_STEP_SIZE = 326.613 # Seconds
+SPLIT_DETECTION_SAMLPING_STEP_SIZE = 772.26 # Seconds
