bf8056379a48114cbfbede38769ba6aebc8bb4ad
nurriol2 <nurriol2@illinois.edu>
2022-05-12 23:59:40 -0500

    Remove reporting printer

--- main.py
+++ main.py
@@ -5,28 +5,6 @@ from setup import build_file_tree
 from data_toolbox.preprocessing import ready_all_ucl
 from data_toolbox.preprocessing import ready_all_cst
 
-def report_printer():
-    from data_toolbox.workflows.ucl_workflow import UCLWorkflow
-    from data_toolbox.workflows.custom_workflow import CustomWorkflow
-
-
-    ## Compare Split Detection Reports ##
-    ucl = UCLWorkflow()
-    cst = CustomWorkflow()
-
-    # Specify the sideband and directional mode
-    SIDEBAND = "neg"
-    MODE = 'x'
-
-    # Analyze heterodyne data
-    ucl.target_heterodyne(SIDEBAND, MODE)
-    cst.target_heterodyne(SIDEBAND, MODE)
-
-    # Estimated coefficients for mechanical frequency should be close
-    print(ucl.freq_report)
-    print(cst.freq_report)
-
-    return
 
 def main():
     
