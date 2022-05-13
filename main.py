b463a83ada2ba5a94768935b1e9852c28d1b9db3
nurriol2 <nurriol2@illinois.edu>
2022-05-12 21:28:58 -0500

    Add current state of main

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from data_toolbox.lorentz import Lorentzian
+import logging
+FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 from setup import build_file_tree
-from data_toolbox.workflows.ucl_workflow import UCLWorkflow
-from data_toolbox.workflows.custom_workflow import CustomWorkflow
+from data_toolbox.preprocessing import find_split_detection_spectra, ready_all_ucl
+from data_toolbox.preprocessing import preprocess_cst_split_detection as cst_split
+from data_toolbox.preprocessing import one_split_detection_cycle
 
-def main():
+def report_printer():
+    from data_toolbox.workflows.ucl_workflow import UCLWorkflow
+    from data_toolbox.workflows.custom_workflow import CustomWorkflow
 
-    # # Setup the experiment data directory no matter what platform this is executed on
-    build_file_tree()
 
-    # ## Compare Split Detection Reports ##
+    ## Compare Split Detection Reports ##
     ucl = UCLWorkflow()
     cst = CustomWorkflow()
 
-    # # Specify the sideband and directional mode
-    SIDEBAND = "pos"
-    MODE = 'y'
+    # Specify the sideband and directional mode
+    SIDEBAND = "neg"
+    MODE = 'x'
 
-    # Analyze heterodyne data 
+    # Analyze heterodyne data
     ucl.target_heterodyne(SIDEBAND, MODE)
     cst.target_heterodyne(SIDEBAND, MODE)
 
@@ -24,33 +27,20 @@ def main():
     print(ucl.freq_report)
     print(cst.freq_report)
 
-    # from data_toolbox.spectra import HeterodyneData
-    # import matplotlib.pyplot as plt
-    # from setup import HETERODYNE_RAW
-
-    # EXAMPLE_HETR = "het_st80_83.CSV"
-    # HETR_UNITS = "V^2/Hz"
-    # zoom = 50
-
-    # hetr = HeterodyneData(HETERODYNE_RAW, EXAMPLE_HETR, HETR_UNITS)
-    
-    # # Worked ok for positive sideband
-    # # hetr.trim_spectrum_to_sideband("pos", 1_000)
-    # # Fit a lorentzian
-    # # lorentzian = hetr.fit_lorentzian("pos", 'y')
-
-
-    # # Worked ok for negative sideband
-    # hetr.trim_spectrum_to_sideband("neg", 200, -1_000)
-    # # Fit a lorentzian
-    # lorentzian = hetr.fit_lorentzian("neg", 'y')
-
-    # plt.plot(hetr.frequencies, hetr.spectrum)
-    # plt.plot(hetr.frequencies, lorentzian.values)
-    # plt.show()
-    
-
     return
+
+def main():
+    build_file_tree()
+    ready_all_ucl()
+    file_list = find_split_detection_spectra('x')
+    logging.debug(f"Num found:  {len(file_list)}")
+    logging.debug(f"List of files:  {file_list[:3]}")
+
+    p = file_list[0]
+    logging.debug(f"Path to a file {p}")
+    l = one_split_detection_cycle(p)
+    logging.debug(f"Lorentzian Object:  {l}")
+    return 
 
 if __name__=="__main__":
     main()
