6812d3eb59d7bab8047443f3a9c423a9fc8c2d97
nurriol2 <nurriol2@illinois.edu>
2022-05-12 23:50:05 -0500

    Finish preprocess steps

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import logging
 FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 from setup import build_file_tree
-from data_toolbox.preprocessing import find_split_detection_spectra, ready_all_ucl
-from data_toolbox.preprocessing import preprocess_cst_split_detection as cst_split
-from data_toolbox.preprocessing import one_split_detection_cycle
+from data_toolbox.preprocessing import ready_all_ucl
+from data_toolbox.preprocessing import ready_all_cst
 
 def report_printer():
     from data_toolbox.workflows.ucl_workflow import UCLWorkflow
@@ -30,16 +29,13 @@ def report_printer():
     return
 
 def main():
+    
     build_file_tree()
+    
     ready_all_ucl()
-    file_list = find_split_detection_spectra('x')
-    logging.debug(f"Num found:  {len(file_list)}")
-    logging.debug(f"List of files:  {file_list[:3]}")
+    ready_all_cst()
 
-    p = file_list[0]
-    logging.debug(f"Path to a file {p}")
-    l = one_split_detection_cycle(p)
-    logging.debug(f"Lorentzian Object:  {l}")
+
     return 
 
 if __name__=="__main__":
